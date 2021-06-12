1c4c7018486b775bdf59df9271fd3e74916eca9c
maxim.vvedensky <dmitrii.bragin90@gmail.com>
2021-06-13 02:23:39 +0300

    first test commit

---
+++ src/main/java/ca/home/novacom/restfull/domain/Car.java
@@ -0,0 +1,8 @@
+package ca.home.novacom.restfull.domain;
+
+public class Car {
+    private String id;
+    private String model;
+    private Drive drive;
+    private String wills;
+}
---
+++ src/main/java/ca/home/novacom/restfull/domain/Drive.java
@@ -0,0 +1,7 @@
+package ca.home.novacom.restfull.domain;
+
+public class Drive {
+    private String id;
+    private Integer v;
+    private String model;
+}
